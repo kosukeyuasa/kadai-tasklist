38a1321f5abca410379376ce96d1934c1d913465
kosuke.yuasa <kosuke.yuasa@rakuten.com>
2018-06-06 06:58:09 +0000

    user pages

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -4,7 +4,24 @@
 
 <div class="row">
    <div class="col-xs-12 col-sm-offset-2 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
-    
+    <aside class="col-xs-4">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">{{ $user->name }}</h3>
+                </div>
+                <div class="panel-body">
+                <img class="media-object img-rounded img-responsive" src="{{ Gravatar::src($user->email, 500) }}" alt="">
+                </div>
+            </div>
+        </aside>
+        <div class="col-xs-8">
+            <ul class="nav nav-tabs nav-justified">
+                <li><a href="#">TimeLine</a></li>
+                <li><a href="#">Followings</a></li>
+                <li><a href="#">Followers</a></li>
+            </ul>
+        </div>
+
  <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
 
     <table class="table table-bordered">
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ Route::resource('tasks', 'TasksController');
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
+Route::group(['middleware' => ['auth']], function () {Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+});
 
 
 
